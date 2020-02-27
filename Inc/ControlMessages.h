993b8274fbf539e8efe14299219deab8f967305b
Brandon <bxk5113@rit.edu>
2020-02-26 23:03:20 -0500

    Added Packet Creation and Parsing to ControlMessages

--- Inc/ControlMessages.h
+++ Inc/ControlMessages.h
@@ -4,9 +4,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-// Special Character Definitions
-#define HEADING_START (0x01)
-#define TRANSMISSION_END (0x04)
+#define PCKT_TX_START     ('{')
+#define PCKT_TX_END       ('}')
+#define PCKT_HEADER_START ('<')
+#define PCKT_HEADER_END   ('>')
+#define PCKT_MTR_START    ('(')
+#define PCKT_MTR_END      (')')
+#define PCKT_SOL_START    ('[')
+#define PCKT_SOL_END      (']')
+
+#define PCKT_SIZE_HEX_MAX (4)
 
 // Enum Definitions for fields
 typedef enum {
@@ -22,67 +29,65 @@ typedef enum {
 } SOL_MVMNT;
 
 typedef enum {
-    PCKT_TYPE_SETUP = 0,
+    PCKT_TYPE_INVALID = 0,
+    PCKT_TYPE_SETUP,
     PCKT_TYPE_INDEX,
     PCKT_TYPE_MVMNT_FIN,
     PCKT_TYPE_ERROR,
     PCKT_TYPE_STOP // Keep as last item
 } PCKT_TYPE;
 
+typedef enum {
+    PCKT_INDEX_TYPE_INVALID,
+    PCKT_INDEX_TYPE_MTR,
+    PCKT_INDEX_TYPE_SOL,
+} PCKT_INDEX_TYPE;
+
 // Packet structures
-typedef struct {
+typedef struct __attribute__((__packed__)){
     char      StartOfHeader;
-    uint32_t  PacketSize;
+    char      PacketSize[PCKT_SIZE_HEX_MAX + 2];
     uint8_t   PacketType;
     char      EndOfHeader;
 } PACKET_HEADER;
 
-typedef struct {
+typedef struct __attribute__((__packed__)){
     char      StartOfPacket;
     uint8_t   Motor;
-    MTR_MVMNT Mvmnt;
+    uint8_t   Mvmnt;
     char      EndOfPacket;
 } PACKET_MTR;
 
-typedef struct {
+typedef struct __attribute__((__packed__)){
     char      StartOfPacket;
     uint8_t   Solenoid;
-    SOL_MVMNT Mvmnt;
+    uint8_t   Mvmnt;
     char      EndOfPacket;
 } PACKET_SOL;
 
-typedef struct {
+typedef struct __attribute__((__packed__)){
     char Tail;
     char NullTerm;
 } PACKET_TAIL;
 
 // Public Functions
-// Packet Formation Functions
-
-/**
-    Creates a packet given a number of motor/solenoid movements.
-    Setup such that the index
-**/
-
-bool CreateIndexPacket(uint8_t* dataOut, int* dataSize, 
-                       PACKET_MTR* mtrPckt, int numMtrPckt, 
-                       PACKET_SOL* solPckt, int numSolPckt);
-PACKET_MTR CreateMtrPacket(uint8_t mtr, MTR_MVMNT mvmnt);
-PACKET_SOL CreateSolPacket(uint8_t sol, SOL_MVMNT mvmnt);
-
-// Packet Parsing Functions
-bool ParseMtrPacket(PACKET_MTR, uint8_t* mtr, MTR_MVMNT* mvmnt);
-bool ParseSolPacket(PACKET_SOL, uint8_t* sol, SOL_MVMNT* mvmnt);
 
 /**
   * Series of functions to put packet data into a buffer.
   * \param dataStart[In] - Start of buffer 
-  * \param dataPtr[Out] - Pointer in buffer after newly written data
+  * \param dataPtr[In/Out] - Reference to index into buffer
   * \
 **/
-bool PacketStart(uint8_t* dataStart, uint8_t *dataPtr);
-bool PacketAddMtrMvmnt(uint8_t* dataStart, uint8_t *dataPtr, MTR_MVMNT Mvmnt, uint8_t mtr);
-bool PacketAddSolMvmnt(uint8_t* dataStart, uint8_t *dataPtr, SOL_MVMNT Mvmnt, uint8_t sol);
-bool PacketTerm(uint8_t* dataStart, uint8_t *dataPtr);
+bool PacketStart(uint8_t* dataStart, uint8_t* dataPtr);
+bool PacketAddMtrMvmnt(uint8_t* dataStart, uint8_t* dataPtr, MTR_MVMNT Mvmnt, uint8_t mtr);
+bool PacketAddSolMvmnt(uint8_t* dataStart, uint8_t* dataPtr, SOL_MVMNT Mvmnt, uint8_t sol);
+bool PacketTerm(uint8_t* dataStart, uint8_t* dataPtr);
+    
+// Packet Parsing Functions
+bool ParsePacketHeader(uint8_t* dataStart, uint8_t* dataPtr, PCKT_TYPE *packetType, uint16_t* size);
+PCKT_INDEX_TYPE ParseIndexType(uint8_t* dataStart);
+bool ParseMtrPacket(PACKET_MTR *pckt, uint8_t* mtr, MTR_MVMNT* mvmnt);
+bool ParseSolPacket(PACKET_SOL *pckt, uint8_t* sol, SOL_MVMNT* mvmnt);
+
     
 #endif
